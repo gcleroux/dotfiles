63e195d156d88661ee9927ba466a4238ff5a5867
Guillaume Cleroux <guicleroux@icloud.com>
2022-11-04 15:41:08 -0400

    Changed icons in file tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,25 +1,38 @@
 require("nvim-tree").setup({
-	sort_by = "case_sensitive",
-	view = {
-		adaptive_size = true,
-		mappings = {
-			list = {
-				{ key = "u", action = "dir_up" },
-				{ key = "h", action = "split" },
-				{ key = "C", action = "cd" },
-				{ key = "v", action = "vsplit" },
-				{ key = ".", action = "toggle_dotfiles" },
-				{ key = "i", action = "toggle_ignored" },
-			},
-		},
-	},
-	renderer = {
-		group_empty = true,
-	},
-	filters = {
-		dotfiles = true,
-	},
-	git = {
-		ignore = false, -- Show ignored files
-	},
+    sort_by = "case_sensitive",
+    view = {
+        adaptive_size = true,
+        mappings = {
+            list = {
+                { key = "u", action = "dir_up" },
+                { key = "h", action = "split" },
+                { key = "C", action = "cd" },
+                { key = "v", action = "vsplit" },
+                { key = ".", action = "toggle_dotfiles" },
+                { key = "i", action = "toggle_ignored" },
+            },
+        },
+    },
+    renderer = {
+        group_empty = true,
+        icons = {
+            glyphs = {
+                git = {
+                    unstaged = "",
+                    staged = "",
+                    unmerged = "",
+                    renamed = "",
+                    untracked = "",
+                    deleted = "",
+                    ignored = "﯏",
+                },
+            },
+        },
+    },
+    filters = {
+        dotfiles = true,
+    },
+    git = {
+        ignore = false, -- Show ignored files
+    },
 })
