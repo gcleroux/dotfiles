bd61560b542e62bc52a5bad83b9ee0d6b1ae7d95
Guillaume Cleroux <guicleroux@icloud.com>
2022-11-04 14:24:41 -0400

    Switch tabs to spaces

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-	return
+    return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,45 +9,45 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		-- Python formatting
-		formatting.isort,
-		formatting.black.with({ extra_args = { "--fast" } }),
-		diagnostics.flake8,
+    debug = false,
+    sources = {
+        -- Python formatting
+        formatting.isort,
+        formatting.black.with({ extra_args = { "--fast" } }),
+        diagnostics.flake8,
 
-		-- Lua formatting
-		formatting.stylua,
+        -- Lua formatting
+        formatting.stylua,
 
-		-- Markdown formatting
-		formatting.markdown_toc,
+        -- Markdown formatting
+        formatting.markdown_toc,
 
-		-- Prettier
-		formatting.prettier,
+        -- Prettier
+        formatting.prettier,
 
-		-- formatting.markdownlint,
+        -- formatting.markdownlint,
 
-		-- JSON formatting
-		-- formatting.jq,
+        -- JSON formatting
+        -- formatting.jq,
 
-		-- YAML formatting
-		-- formatting.yamlfmt,
+        -- YAML formatting
+        -- formatting.yamlfmt,
 
-		-- C/C++/CS/CUDA formatting
-		formatting.clang_format,
-	},
-	-- you can reuse a shared lspconfig on_attach callback here
-	on_attach = function(client, bufnr)
-		if client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
+        -- C/C++/CS/CUDA formatting
+        formatting.clang_format,
+    },
+    -- you can reuse a shared lspconfig on_attach callback here
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.format({ bufnr = bufnr })
+                end,
+            })
+        end
+    end,
 })
