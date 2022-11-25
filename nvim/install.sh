cce2c0cbe58da075c276f203059cff6d3a7f27ec
Guillaume Cleroux <guicleroux@icloud.com>
2022-11-25 00:29:41 -0500

    Install node 18.x in script

--- nvim/install.sh
+++ nvim/install.sh
@@ -11,70 +11,73 @@ b_CWAR="\033[1;33m" # bold warning color
 
 # echo like ...  with  flag type  and display message  colors
 prompt() {
-	case ${1} in
-	"-s" | "--success")
-		echo -e "${b_CGSC}${@/-s/}${CDEF}"
-		;; # print success message
-	"-e" | "--error")
-		echo -e "${b_CRER}${@/-e/}${CDEF}"
-		;; # print error message
-	"-w" | "--warning")
-		echo -e "${b_CWAR}${@/-w/}${CDEF}"
-		;; # print warning message
-	"-i" | "--info")
-		echo -e "${b_CCIN}${@/-i/}${CDEF}"
-		;; # print info message
-	*)
-		echo -e "$@"
-		;;
-	esac
+    case ${1} in
+        "-s" | "--success")
+            echo -e "${b_CGSC}${@/-s/}${CDEF}"
+            ;; # print success message
+        "-e" | "--error")
+            echo -e "${b_CRER}${@/-e/}${CDEF}"
+            ;; # print error message
+        "-w" | "--warning")
+            echo -e "${b_CWAR}${@/-w/}${CDEF}"
+            ;; # print warning message
+        "-i" | "--info")
+            echo -e "${b_CCIN}${@/-i/}${CDEF}"
+            ;; # print info message
+        *)
+            echo -e "$@"
+            ;;
+    esac
 }
 
 # Installing neovim bin from source
 nvim_install() {
-	# Installing neovim from bin
-	NVIM_VERSION=$(curl -s "https://api.github.com/repos/neovim/neovim/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v*([^"]+)".*/\1/')
-	curl -LO "https://github.com/neovim/neovim/releases/download/v$NVIM_VERSION/nvim-linux64.deb"
-	curl -LO "https://github.com/neovim/neovim/releases/download/v$NVIM_VERSION/nvim-linux64.deb.sha256sum"
-	if [ "$(sha256sum -c nvim-linux64.deb.sha256sum)" != "nvim-linux64.deb: OK" ]; then
-		prompt -e "\n Neovim checksum failed."
-		exit 1
-	fi
-	sudo apt install -y ./nvim-linux64.deb
-	rm ./nvim-linux64.deb ./nvim-linux64.deb.sha256sum
+    # Installing neovim from bin
+    NVIM_VERSION=$(curl -s "https://api.github.com/repos/neovim/neovim/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v*([^"]+)".*/\1/')
+    curl -LO "https://github.com/neovim/neovim/releases/download/v$NVIM_VERSION/nvim-linux64.deb"
+    curl -LO "https://github.com/neovim/neovim/releases/download/v$NVIM_VERSION/nvim-linux64.deb.sha256sum"
+    if [ "$(sha256sum -c nvim-linux64.deb.sha256sum)" != "nvim-linux64.deb: OK" ]; then
+        prompt -e "\n Neovim checksum failed."
+        exit 1
+    fi
+    sudo apt install -y ./nvim-linux64.deb
+    rm ./nvim-linux64.deb ./nvim-linux64.deb.sha256sum
 }
 
 # Checking for root access and proceed if it is present
 if [ "$UID" != "$ROOT_UID" ]; then
-	# Error message
-	prompt -e "This script should be run as root.\n"
-	prompt -e "Operation canceled."
-	exit 1
+    # Error message
+    prompt -e "This script should be run as root.\n"
+    prompt -e "Operation canceled."
+    exit 1
 fi
 
 # Updating repo
 sudo apt update -y
 
 # Installing build deps
-sudo apt install -y ripgrep npm curl
+sudo apt install -y ripgrep curl
+
+# Installing node 18.x
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash - && apt-get install -y nodejs
 
 # Installing neovim
 prompt -i "\nInstalling Neovim bin from source.\n"
 nvim_install
 
 # Installing add-ons
-read -p "
+read -rp "
 Do you wish to install neovim's add-ons? [Y/n] " yn
 
 case $yn in
-# Default to yes
-[Yy]* | "")
-	cd ./add-ons || exit 1
-	bash ./install.sh
-	;;
-*)
-	prompt -w "\nNeovim add-ons will not be installed.\n"
-	;;
+        # Default to yes
+    [Yy]* | "")
+        cd ./add-ons || exit 1
+        bash ./install.sh
+        ;;
+    *)
+        prompt -w "\nNeovim add-ons will not be installed.\n"
+        ;;
 esac
 
 prompt -s "Neovim was installed sucessfully!"
